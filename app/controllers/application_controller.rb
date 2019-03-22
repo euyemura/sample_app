58f1b4179804bde41fa21e98d69357a8025953e8
euyemura <ea.uyemura@gmail.com>
2019-03-22 15:10:50 -0700

    did some cleanup and looked over the code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world"
+    # render html: "hello, world"
+    render(:html => "why hello there")
+    puts "hello this is self, #{self.is_a? ApplicationController}"
+    #so self is the ApplicationController object, i dont think its actually the class. yes it's just an instance of ApplicationController
+    # see here, really what the original was doing was just calling a method called render, and now i want to output what exactly self is, because thats what render should be being called upon.
   end
 end
