210680f77c83b65fa1b6d3b399389e1cd951b9a5
euyemura <ea.uyemura@gmail.com>
2019-03-31 14:15:33 -0700

    changed path names and filled in all inactive links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/help'
+  get '/help' => "static_pages#help"
 
-  get 'static_pages/about'
+  get '/about', to: "static_pages#about"
+  # these are variations of each other.
 
-  get 'static_pages/contact'
+  get '/contact', :to => "static_pages#contact"
+  # i wonder if this last variation will work, the full hash syntax.
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
