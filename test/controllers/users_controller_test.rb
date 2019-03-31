9841c0e7c51b288f05b39175687cab178ba2dc23
euyemura <ea.uyemura@gmail.com>
2019-03-31 15:30:15 -0700

    created tests for the sign up route, and changed the route name

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   test "should get new" do
-    get users_new_url
+    puts "testing the sign up page"
+    get signup_path
     assert_response :success
+    puts "title should be #{full_title('Sign up')}"
+    assert_select "title", full_title('Sign up')
   end
 
 end
