c7a0829af32ee41ccd28f2c27e4835e07f91a512
euyemura <ea.uyemura@gmail.com>
2019-05-10 14:43:10 -0700

    Implemented the redirect to show page once a new user is created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
+      # same as redirect_to user_path(@user)
     else
       render 'new'
     end
