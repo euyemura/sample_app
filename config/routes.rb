8897eb0bc34a316857356a8084f86af1088fb4a2
ak-ericuyemura <eric.uyemura@annkissam.com>
2019-06-10 15:51:31 -0400

    implemented the prototype log in utilizing a Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get '/signup', to: "users#new"
   post '/signup', to: "users#create"
 
@@ -14,6 +16,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/hello' => "application#hello"
-
+  
+  get '/login', to: 'sessions#new' 
+  post '/login', to: 'sessions#create' 
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
